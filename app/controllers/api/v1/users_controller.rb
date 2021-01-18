6a61a5d92cc75c560dab71e3a8eb3a85b9d42621
Connor Ferguson <cpfergus1@gmail.com>
2021-01-18 11:14:34 -0700

    Implemented user create api point

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,26 @@
+
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        user = User.new(user_params)
+        if user.save
+          render json: UserSerializer.new(user), status: :created
+        else
+          render json: generate_error(user), status: :bad_request
+        end
+      end
+
+      private
+
+      def user_params
+        params[:api_key] = SecureRandom.base64(14)
+        params.permit(:email, :password, :password_confirmation, :api_key)
+      end
+
+      def generate_error(user)
+        { messages: user.errors.full_messages.to_sentence}
+      end
+    end
+  end
+end
---
+++ spec/requests/api/v1/user_request_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'User API' do
+  it 'it can create a user' do
+    headers = {'CONTENT_TYPE' => 'application/json'}
+    json_payload = { email: "email@email.com",
+                    password: "abc123",
+                    password_confirmation: "abc123" }
+    post '/api/v1/users', headers: headers, params: JSON.generate(json_payload)
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+    json = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(json).to_not have_key(:password)
+    expect(json[:attributes]).to_not have_key(:password)
+    expect(json[:attributes][:api_key]).to be_a(String)
+    user = User.last
+    expect(user.password_digest).to_not eq('abc123')
+  end
+
+  it 'returns an error if user creation fails' do
+    headers = {'CONTENT_TYPE' => 'application/json'}
+    json_payload = { email: "email@email.com",
+                    password: "abc123",
+                    password_confirmation: "abc321" }
+    post '/api/v1/users', headers: headers, params: JSON.generate(json_payload)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(400)
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json[:messages]).to eq("Password confirmation doesn't match Password")
+  end
+end
