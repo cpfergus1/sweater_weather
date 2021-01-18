35eef3c2b8453943874253ce478b463721655049
Connor Ferguson <cpfergus1@gmail.com>
2021-01-17 19:09:37 -0700

    implemented image service tests

---
+++ spec/services/image_service_spec.rb
@@ -0,0 +1,47 @@
+describe '.class_methods' do
+  it 'image_search method returns parsed response based on params', :vcr do
+    params = {
+      location: 'denver,co'
+    }
+    search = ImageService.image_search(params)
+
+    expect(search).to be_a(Hash)
+    expect(search.keys).to eq([:_type,
+                              :instrumentation,
+                              :readLink,
+                              :webSearchUrl,
+                              :queryContext,
+                              :totalEstimatedMatches,
+                              :nextOffset,
+                              :currentOffset,
+                              :value,
+                              :queryExpansions,
+                              :pivotSuggestions,
+                              :relatedSearches])
+    expect(search[:value]).to be_an(Array)
+    expect(search[:value].count).to eq(5)
+    expect(search[:value][0]).to be_a(Hash)
+    expect(search[:value][0].keys).to eq([:webSearchUrl,
+                                        :name,
+                                        :thumbnailUrl,
+                                        :datePublished,
+                                        :isFamilyFriendly,
+                                        :contentUrl,
+                                        :hostPageUrl,
+                                        :contentSize,
+                                        :encodingFormat,
+                                        :hostPageDisplayUrl,
+                                        :width,
+                                        :height,
+                                        :hostPageFavIconUrl,
+                                        :hostPageDomainFriendlyName,
+                                        :hostPageDiscoveredDate,
+                                        :thumbnail,
+                                        :imageInsightsToken,
+                                        :insightsMetadata,
+                                        :imageId,
+                                        :accentColor]
+                                        )
+    expect(search[:value][0][:contentUrl]).to be_a(String)
+  end
+end
