3a9126a58ab496aabb5f15395893a94b10e6aef7
Connor Ferguson <cpfergus1@gmail.com>
2021-01-19 10:20:50 -0700

    updated muncies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class MunchiesController < ApplicationController
       def index
-        munchies_info = BusinessService.search_buisness(request_params)
+        munchies_info = BusinessService.search_business(request_params)
         travel_info = RoutesFacade.search_routes(request_params)
         munchies = Munchies.new(munchies_info, travel_info)
         render json: MunchiesSerializer.new(munchies), status: :ok
