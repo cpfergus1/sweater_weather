fdd37318895c64339471047c2f77606994fd9c3a
Connor Ferguson <cpfergus1@gmail.com>
2021-01-19 10:04:59 -0700

    build hypothetical controller

---
+++ app/controllers/api/v1/munchies_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module V1
+    class MunchiesController < ApplicationController
+      def index
+        munchies_info = BusinessService.search_buisness(request_params)
+        travel_info = RoutesFacade.search_routes(request_params)
+        munchies = Munchies.new(munchies_info, travel_info)
+        render json: MunchiesSerializer.new(munchies), status: :ok
+      end
+
+      private
+
+      def request_params
+        params[:units] = 'metric' unless params[:units]
+        params.permit(:origin, :destination, :food, :units)
+      end
+    end
+  end
+end
