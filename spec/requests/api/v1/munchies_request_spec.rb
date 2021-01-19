75a91d66a22705fc8d470ab53954a6df1904bfe9
Connor Ferguson <cpfergus1@gmail.com>
2021-01-19 11:44:16 -0700

    implemented more sad path testing on munchies controller

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -50,9 +50,35 @@ describe 'Munchies API' do
     expect(json).to_not be_empty
   end
 
-  it "returns an error for no food input", :vcr do
+  it "returns an random restaurant for no food input", :vcr do
 
-    get '/api/v1/munchies?origin=,co&destination=&food=&units=imperial'
+    get '/api/v1/munchies?origin=,co&destination=denver&food=&units=imperial'
+    expect(response).to be_successful
+    munchies = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(munchies).to be_a(Hash)
+    expect(munchies[:attributes]).to be_a(Hash)
+    expect(munchies[:attributes][:destination_city]).to be_a(String)
+    expect(munchies[:attributes][:travel_time]).to be_a(String)
+    expect(munchies[:attributes][:weather_at_eta]).to be_a(Hash)
+    expect(munchies[:attributes][:weather_at_eta][:temperature]).to be_a(Float)
+    expect(munchies[:attributes][:weather_at_eta][:conditions]).to be_a(String)
+    expect(munchies[:attributes][:restaurant]).to be_a(Hash)
+    expect(munchies[:attributes][:restaurant][:name]).to be_a(String)
+    expect(munchies[:attributes][:restaurant][:address]).to be_a(String)
+  end
+
+  it "returns an error for bad food input", :vcr do
+
+    get '/api/v1/munchies?origin=,co&destination=pueblo&food=dondondon&units=imperial'
+    expect(response).to_not be_successful
+    expect(response.status).to be >= 400
+    json = JSON.parse(response.body, symbolize_names: true)
+    expect(json).to_not be_empty
+  end
+
+  it "returns an error for bad start origin", :vcr do
+
+    get '/api/v1/munchies?origin=&destination=pueblo&food=chinese&units=imperial'
     expect(response).to_not be_successful
     expect(response.status).to be >= 400
     json = JSON.parse(response.body, symbolize_names: true)
