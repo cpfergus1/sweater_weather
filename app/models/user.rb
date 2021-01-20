93153f85981c6b142d90a3a663363f87909b451d
Connor Ferguson <cpfergus1@gmail.com>
2021-01-20 09:12:51 -0700

    fixed bug where user could create account with non email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, uniqueness: true, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, on: :create
 end
